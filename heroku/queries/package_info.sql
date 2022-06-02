5144ba92e384726b813e62864d547c3197aec137
Thomas Li <47963215+lithomas1@users.noreply.github.com>
2022-06-01 20:36:35 -0700

    Add backend and simple GUI

---
+++ heroku/queries/package_info.sql
@@ -0,0 +1,18 @@
+-- Sometimes we can't figure out the version, platform tags, etc. from the filename.
+-- This happens when we don't have a wheel or sdist(any file that doesn't end in .tar.gz, .zip, or .whl)
+-- We need to pull info from the table manually in that case.
+SELECT
+    CASE
+        WHEN REGEXP_CONTAINS (file.filename, r'^.*\.(tar.gz|zip|whl)')
+        THEN file.filename
+        ELSE CONCAT(file.filename, ' ', file.version, ' ', file.type)
+    END
+        AS file, count(*) AS download_counts
+
+
+FROM `bigquery-public-data.pypi.file_downloads`
+WHERE project = '$projectname'
+AND DATE(timestamp)
+    BETWEEN DATE_SUB(CURRENT_DATE(), INTERVAL 1 DAY)
+    AND CURRENT_DATE()
+GROUP BY file
